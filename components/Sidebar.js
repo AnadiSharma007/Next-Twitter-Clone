c59f6253de93c7d74edd01d29585848e656a3a18
AnadiSharma007 <anadis764@gmail.com>
2022-06-14 18:57:05 +0530

    build sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,8 +11,12 @@ import {
     DotsHorizontalIcon,
   } from "@heroicons/react/outline";
 import SidebarLink from './SidebarLink';
+import { signOut, useSession } from 'next-auth/react';
 
 const Sidebar = () => {
+
+    const { data: session } = useSession();
+    
   return (
     <div className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
         <div className='flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24'>
@@ -31,11 +35,11 @@ const Sidebar = () => {
         <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
             Tweet
         </button>
-        <div className='text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto mt-auto xl:-mr-auto'>
-            <img src="https://avatars.githubusercontent.com/u/60505922?v=4" alt="" className='h-10 w-10 rounded-full xl:mr-2.5' />
+        <div className='text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto mt-auto xl:-mr-auto' onClick={signOut}>
+            <img src={session.user.image} alt="" className='h-11 w-11 rounded-full xl:mr-2.5' />
             <div className="hidden xl:inline leading-5">
-                <h4 className='font-bold'>Anadi 1224</h4>
-                <p className='text-[#6e767d]'>@anadi1224</p>
+                <h4 className='font-bold'>{session.user.name}</h4>
+                <p className='text-[#6e767d]'>@{session.user.tag}</p>
             </div>
             <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
         </div>
